7c68b3f686675b65075c281dce8ec59f85fbd0c6
Fionera <fionera@fionera.de>
2019-07-19 12:57:54 +0200

    Add correct number parsing

--- Controllers/Frontend/OstQrCodeReader.php
+++ Controllers/Frontend/OstQrCodeReader.php
@@ -66,6 +66,20 @@ class Shopware_Controllers_Frontend_OstQrCodeReader extends Enlight_Controller_A
             $path = parse_url($q)['path'];
             $number = array_reverse(explode('/', $path))[0];
             $q = $number;
+        } else {
+            $re = '/^\d[0]+(\d+)(\d{5})\d$/m';
+
+            preg_match_all($re, $q, $matches, PREG_SET_ORDER);
+            if (count($matches) > 0) {
+                [,$number, $variantNumber] = $matches[0];
+
+                $variantNumber = ltrim($variantNumber, '0');
+                if (trim($variantNumber, '0') !== '') {
+                    $number .= '-' . $variantNumber;
+                }
+
+                $q = $number;
+            }
         }
 
         $this->forward('index', 'search', 'frontend', ['sSearch' => $q]);
